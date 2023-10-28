8546d0e4ccb60e3672e91d0fef06d7d2e1bd6038
thisis-arman <armanhossen1549@gmail.com>
2023-10-28 10:04:09 +0600

    first commit

---
+++ src/app/Components/HomeComponents/Banner.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+            <section>
+
+            </section>
+
+            {/*  <section className="bg-gray-50">
+                <div
+                    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+                >
+                    <div className="mx-auto max-w-xl text-black text-center">
+                        <h1 className="text-3xl font-extrabold sm:text-5xl">
+                            Understand User Flow.
+                            <strong className="font-extrabold text-yellow-700 sm:block">
+                                Increase Conversion.
+                            </strong>
+                        </h1>
+
+                        <p className="mt-4 sm:text-xl/relaxed">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                            tenetur fuga ducimus numquam ea!
+                        </p>
+
+                        <div className="mt-8 flex flex-wrap justify-center gap-4">
+                            <a
+                                className="block w-full rounded bg-yellow-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-yellow-700 focus:outline-none focus:ring active:bg-yellow-500 sm:w-auto"
+                                href="/get-started"
+                            >
+                                Get Started
+                            </a>
+
+                            <a
+                                className="block w-full rounded px-12 py-3 text-sm font-medium text-yellow-600 shadow hover:text-yellow-700 focus:outline-none focus:ring active:text-yellow-500 sm:w-auto"
+                                href="/about"
+                            >
+                                Learn More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section> */}
+
+        </div>
+    );
+};
+
+export default Banner;
