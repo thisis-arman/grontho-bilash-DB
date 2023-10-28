7d30c3ec7739f5546843a80eb5dbbf5a484b6e16
thisis-arman <armanhossen1549@gmail.com>
2023-10-28 13:53:34 +0600

    tabs for category

--- src/app/(MainLayout)/page.js
+++ src/app/(MainLayout)/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Banner from '../Components/HomeComponents/Banner'
 import Nav from '../Components/HomeComponents/Nav'
+import Tabs from '../Components/HomeComponents/Tabs'
 
 export default function Home() {
   return (
    <main>
     <Nav/>
    <Banner/>
+   <Tabs/>
    </main>
   )
 }
