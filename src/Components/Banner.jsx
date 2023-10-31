10fc02abfcd61fa62dfac90ed9b44fd3a4d3ebdf
thisis-arman <armanhossen1549@gmail.com>
2023-10-31 23:33:46 +0600

    banner overlay text

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -24,11 +24,20 @@ const Banner = () => {
         })
     }, [])
     return (
-        <div className=''>
-            <Swiper pagination={true} modules={[Pagination]} >
+        <div className='h-screen'>
+            <Swiper pagination={true} modules={[Pagination]} className='h-screen' >
                 <SwiperSlide className='' >
 
-                    <img data-aos="fade-right" data-aos-delay="100" className='object-cover w-full h-full object-center ' src="https://demo.fieldthemes.com/bookshop/demo10/modules/revsliderprestashop/uploads/slider1-h10.jpg" alt="banner 01" />
+                    <div className=''>
+                        <img data-aos="fade-right" data-aos-delay="200" className='md:object-cover w-full h-full md:object-center ' src="https://demo.fieldthemes.com/bookshop/demo10/modules/revsliderprestashop/uploads/slider1-h10.jpg" alt="banner 01" />
+                        <div className='relative -top-80  -right-[700px]' data-aos="fade-up" data-aos-delay="100">
+                            <span className='text-5xl font-bold'>Grontho Bilash</span>
+                            <p className='max-w-xl py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae dicta et debitis laudantium, at maxime.</p>
+                            <button data-aos="fade-up" data-aos-delay="200" className='border my-4 hover:bg-[#11B8B4] rounded px-6 py-2 text-black hover:text-white transition p-1'>Shop Now</button>
+
+
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src="https://demo.fieldthemes.com/bookshop/demo10/modules/revsliderprestashop/uploads/slider2-h10.jpg" alt="banner 01" />
